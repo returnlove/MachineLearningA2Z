e6c04ce49b51bc4d8fe985f7c5ebae9c2b5a16d2
returnlove <iamsainath2@gmail.com>
2017-12-26 15:11:31 +0530

    kmeans in R

--- Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans.R
+++ Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans.R
@@ -1,47 +1,27 @@
-# K-Means Clustering
+rm(list = ls())
+# K_means clustering
 
-# Importing the dataset
-dataset = read.csv('Mall_Customers.csv')
-dataset = dataset[4:5]
+dataset = read.csv("Mall_Customers.csv")
+View(dataset)
 
-# Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
-# library(caTools)
-# set.seed(123)
-# split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
-# training_set = subset(dataset, split == TRUE)
-# test_set = subset(dataset, split == FALSE)
+x <- dataset[,4:5]
 
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
+#elbow method to find optimal K
+set.seed(1)
 
-# Using the elbow method to find the optimal number of clusters
-set.seed(6)
-wcss = vector()
-for (i in 1:10) wcss[i] = sum(kmeans(dataset, i)$withinss)
-plot(1:10,
-     wcss,
-     type = 'b',
-     main = paste('The Elbow Method'),
-     xlab = 'Number of clusters',
-     ylab = 'WCSS')
+?kmeans()
 
-# Fitting K-Means to the dataset
-set.seed(29)
-kmeans = kmeans(x = dataset, centers = 5)
-y_kmeans = kmeans$cluster
+wcss <- vector()
+for (i in 1:10)
+  wcss[i] <- sum(kmeans(x, i)$withinss)
 
-# Visualising the clusters
+plot(1:10, wcss, type = "b", xlab = "number of clusters", ylab = "#wcss")
+
+#k = 5
+
+set.seed(2)
+kmeans <- kmeans(x, 5)
+
 library(cluster)
-clusplot(dataset,
-         y_kmeans,
-         lines = 0,
-         shade = TRUE,
-         color = TRUE,
-         labels = 2,
-         plotchar = FALSE,
-         span = TRUE,
-         main = paste('Clusters of customers'),
-         xlab = 'Annual Income',
-         ylab = 'Spending Score')
+clusplot(x, kmeans$cluster, lines = 0, shade =  TRUE, color = TRUE, labels = 2, plotchar = FALSE, span = TRUE, main = paste("Clusters of client"), xlab = "Annual income", ylab = "spending score")
+
